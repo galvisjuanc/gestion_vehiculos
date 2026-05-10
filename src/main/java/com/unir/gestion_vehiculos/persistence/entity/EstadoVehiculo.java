49ae9fbfc25a27d2f3c7d43c81e6c9f0d1641ca3
Juan Galvis <Galvis>
2026-05-10 11:01:03 -0500

    first commit

---
+++ src/main/java/com/unir/gestion_vehiculos/persistence/entity/EstadoVehiculo.java
@@ -0,0 +1,6 @@
+package com.unir.gestion_vehiculos.persistence.entity;
+
+public enum EstadoVehiculo {
+    DISPONIBLE,
+    NO_DISPONIBLE
+}
---
+++ src/test/java/com/unir/gestion_vehiculos/GestionVehiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unir.gestion_vehiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionVehiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
